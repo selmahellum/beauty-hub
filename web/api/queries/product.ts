7f67a985ff4928b1aceb202cf12b1d07e14faca0
Selma Hellum <selma.hellum@me.com>
2025-12-09 22:17:07 +0100

    Sort products alphabetically

--- web/api/queries/product.ts
+++ web/api/queries/product.ts
@@ -1,6 +1,6 @@
 import { groq } from 'next-sanity';
 
-export const GET_PRODUCTS = groq`*[_type == "product"] | order(_createdAt desc) {
+export const GET_PRODUCTS = groq`*[_type == "product"] | order(name asc) {
   _id, 
   name, 
   description,
@@ -12,7 +12,7 @@ export const GET_PRODUCTS = groq`*[_type == "product"] | order(_createdAt desc) 
   _createdAt
 }`;
 
-export const GET_PRODUCTS_BY_CATEGORY = groq`*[_type == "product" && ($category == "all" || category == $category)] | order(_createdAt desc) {
+export const GET_PRODUCTS_BY_CATEGORY = groq`*[_type == "product" && ($category == "all" || category == $category)] | order(name asc) {
   _id, 
   name, 
   description,
